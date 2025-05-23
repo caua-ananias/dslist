bf04129da435092e43cb72a0721745f52e450d87
Caua Ananias <caua.ananias2004@gmail.com>
2025-05-22 21:26:04 -0400

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Games.java
+++ src/main/java/com/devsuperior/dslist/entities/Games.java
@@ -17,23 +17,29 @@ public class Games {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
-    private String longDecription;
+
+    @Column(columnDefinition = "TEXT")
+    private String longDescription;
 
     public Games() {
     }
 
-    public Games(Long id, String title, Integer year, String genre, String platform, String imgUrl, String shortDescription, String longDecription) {
+    public Games(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
-        this.longDecription = longDecription;
+        this.longDescription = longDescription;
     }
 
     public Long getId() {
@@ -68,12 +74,20 @@ public class Games {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platform) {
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
@@ -92,12 +106,12 @@ public class Games {
         this.shortDescription = shortDescription;
     }
 
-    public String getLongDecription() {
-        return longDecription;
+    public String getLongDescription() {
+        return longDescription;
     }
 
-    public void setLongDecription(String longDecription) {
-        this.longDecription = longDecription;
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
     }
 
     @Override
